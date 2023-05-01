36feb7e8ff1968cc1ac81c7eb8a124440208a507
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-05-01 12:39:28 -0500

    building front end

---
+++ react-app/src/App.js
@@ -0,0 +1,26 @@
+
+function App() {
+  return (
+    <div className="App">
+      <section className="side-bar">
+          <button>+ New chat</button>
+          <ul className="history">
+              <li>mmmm</li>
+          </ul>
+          <nav>
+              <p>Made by Cyn</p>
+          </nav>
+      </section>
+      <section className="main">
+          <h1>CynGPT</h1>
+          <ul className="feed">
+
+          </ul>
+      </section>
+
+
+    </div>
+  );
+}
+
+export default App;
