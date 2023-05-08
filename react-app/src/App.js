1ca57bfa7ac00e625fcb94541e08b699dbcaa142
Cynthia Gallegos <cynthialgallegos97@gmail.com>
2023-05-08 12:05:59 -0500

    backend and frontend

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,25 +1,98 @@
+import {useState, useEffect} from 'react'
+const App = () => {
 
-function App() {
+    const [value, setValue] = useState(null)
+    const [message, setMessage] = useState(null)
+    const [previousChats, setPreviousChats] = useState([])
+    const [currentTitle, setCurrentTitle] = useState(null)
+
+
+    const createNewChat = () => {
+        setMessage(null)
+        setValue('')
+        setCurrentTitle(null)
+    }
+
+    const handleClick = (uniqueTitle) => {
+        setCurrentTitle(uniqueTitle)
+        setMessage(null)
+        setValue('')
+    }
+
+    const getMessages = async () => {
+        const options = {
+            method: "POST",
+            body: JSON.stringify({
+                message: value
+            }),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        }
+
+        try {
+            const response = await fetch('http://localhost:5000/completions', options)
+            const data = await response.json()
+            console.log(data)
+            setMessage(data.choices[0].message)
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    console.log(message)
+
+    useEffect(() => {
+console.log(currentTitle, value, message)
+        if (!currentTitle && value && message) {
+            setCurrentTitle(value)
+        }
+        if (currentTitle && value && message) {
+            setPreviousChats(prevChats => (
+                [...prevChats,
+                    {
+                    title: currentTitle,
+                        role: "user",
+                        content: value
+                },
+                 {
+                     title: currentTitle,
+                     role: message.role,
+                     content: message.content
+                 }
+                ]
+            ))
+        }
+    }, [message, currentTitle])
+
+    console.log(previousChats)
+
+    const currentChat = previousChats.filter(previousChat => previousChat.title === currentTitle);
+    const uniqueTitles = Array.from(new Set(previousChats.map(previousChat => previousChat.title)))
+
   return (
     <div className="app">
       <section className="side-bar">
-          <button>+ New chat</button>
+          <button onClick={createNewChat}>+ New chat</button>
           <ul className="history">
-              <li>mmmm</li>
+              {uniqueTitles?.map((uniqueTitle, index) => <li key={index} onClick={() => handleClick(uniqueTitle)}>{uniqueTitle}</li>)}
           </ul>
           <nav>
               <p>Made by Cyn</p>
           </nav>
       </section>
       <section className="main">
-          <h1>CynGPT</h1>
+          {!currentTitle && <h1>CynGPT</h1>}
           <ul className="feed">
-
+              {currentChat?.map((chatMessage, index) => <li key={index}>
+                  <p className="role">{chatMessage.role}</p>
+                  <p>{chatMessage.content}</p>
+              </li>)}
           </ul>
           <div className="bottom-section">
               <div className="input-containter">
-                  <input/>
-                  <div id="submit">➢</div>
+                  <input value={value} onChange={(e) => setValue(e.target.value)}/>
+                  <div id="submit" onClick={getMessages}>➢</div>
               </div>
               <p className="info">
                   Chat GPT Mar 14 Version. Free Research Preview.
